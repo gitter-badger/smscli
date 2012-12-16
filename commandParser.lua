331a4f7f349e5dce77665179f491ff43b37dedd5
Patrick Malatack <pmala5@gmail.com>
2012-12-16 14:35:15 -0800

    Adding in command parsing logic

---
+++ commandParser.lua
@@ -0,0 +1,40 @@
+local parseCommand
+parseCommand = function(body)
+	index =  string.find(body, " ")
+	if not (index == nil) then
+		command = string.sub(body,1,index-1)
+		rest = string.sub(body, index+1)
+		
+		if string.upper(command) == "CALL" then
+				return {command = "CALL", number = "+14152839902", body = "calltest"}
+		elseif string.upper(command) == "SMS" then
+				return {command = "SMS", number = "+14152839902", body = "smstest"}
+		elseif command == "." then
+				return {command = "SMS", number = storage.lastOutbound, body = rest}
+		elseif command == "#" then
+				return {command = "SMS", number = storage.lastInbound, body = rest}
+		elseif not (findNumber(body).number == nil) then
+				return {command = "SMS", number = findNumber(body).number, body = findNumber(body).body}
+		end		
+	end
+	return {command = "SMS", number = storage.lastOutbound, body = body}
+end
+
+-- this fuction finds a number as the first word in a string 
+-- and returns it or plus the body or nothing
+findNumber = function(body)
+	index =  string.find(body, " ")
+	if not (index == nil) then
+		number = string.sub(body,1,index-1)
+		rest = string.sub(body, index+1)
+		
+		if tonumber(number) and string.len(number) > 4 then
+			return {body = rest, number = number}
+		elseif string.sub(number,1,1) == "+"  and string.len(number) > 4 then
+			if tonumber(string.sub(number,2)) then			
+				return {body = rest, number = number}
+			end
+		end
+	end
+	return {body = body}
+end
